545a18960b348fb30f192123987b455e107208b3
yavilar <yavilarobledo@ivytech.edu>
2024-04-09 23:07:54 -0400

    tried

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,70 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
+from b2 import B2
+
+
+
+# ------------------------------------------------------
+#                      APP CONSTANTS
+# ------------------------------------------------------
+REMOTE_DATA = 'apartments_for_rent_classified_100K.csv'
+
+
+# ------------------------------------------------------
+#                        CONFIG
+# ------------------------------------------------------
+load_dotenv()
+
+# load Backblaze connection
+b2 = B2(endpoint=os.environ['B2_ENDPOINT'],
+        key_id=os.environ['B2_KEYID'],
+        secret_key=os.environ['B2_APPKEY'])
+
+
+# ------------------------------------------------------
+#                        CACHING
+# ------------------------------------------------------
+@st.cache_data
+def get_data():
+    # collect data frame of reviews and their sentiment
+    b2.set_bucket(os.environ['B2_BUCKETNAME'])
+    df_apartment= b2.get_df(REMOTE_DATA)
+
+    # average sentiment scores for the whole dataset
+    benchmarks = df_coffee[['neg', 'neu', 'pos', 'compound']] \
+                    .agg(['mean', 'median'])
+    
+    return df_coffee, benchmarks
+
+
+@st.cache_resource
+
+
+
 import streamlit as st
 import pandas as pd
 
-# Load the dataset
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00555/ApartmentData.zip"
-df = pd.read_csv(url, compression='zip')
 
 # Sidebar title
 st.sidebar.title("Filters")
 
 # Filter by location
-locations = df['Location'].unique()
+locations = df_apartment['Location'].unique()
 selected_location = st.sidebar.selectbox("Select Location", locations)
 
 # Filter by number of bedrooms
-bedrooms = df['NumberOfBedrooms'].unique()
+bedrooms = df_apartment['NumberOfBedrooms'].unique()
 selected_bedrooms = st.sidebar.selectbox("Select Number of Bedrooms", bedrooms)
 
 # Filter by number of bathrooms
-bathrooms = df['NumberOfBathrooms'].unique()
+bathrooms = df_apartment['NumberOfBathrooms'].unique()
 selected_bathrooms = st.sidebar.selectbox("Select Number of Bathrooms", bathrooms)
 
 # Filter data based on selected criteria
-filtered_df = df[(df['Location'] == selected_location) &
-                 (df['NumberOfBedrooms'] == selected_bedrooms) &
-                 (df['NumberOfBathrooms'] == selected_bathrooms)]
+filtered_df = df_apartment[(df_apartment['Location'] == selected_location) &
+                 (df_apartment['NumberOfBedrooms'] == selected_bedrooms) &
+                 (df_apartment['NumberOfBathrooms'] == selected_bathrooms)]
 
 # Display filtered data
 st.write(filtered_df)
